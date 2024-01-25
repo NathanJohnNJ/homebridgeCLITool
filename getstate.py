8b08ff4ed935f6b0427202e93fbea77d879d56ac
Nathan John <codesnj@gmail.com>
2024-01-25 06:06:34 +0000

    Added more notes

--- getstate.py
+++ getstate.py
@@ -1,22 +1,25 @@
-import sys
-import configparser
-import requests
+import sys #importing sys to pass arguments to the script i.e. device to be controlled.
+import configparser #allows to read the config.ini file in order to get the correct ID for the given device as this is needed to control it
+import requests # allows to make the API request
 import urllib.parse
 import json
-from dotenv import load_dotenv
+from dotenv import load_dotenv # hide sensitive information
 import os #provides ways to access the Operating System and allows us to read the environment variables
 
 load_dotenv()
-
+#set variable to be used throughout from the dotenv file
 myUser = os.getenv("USERNAME")
 myPass = os.getenv("PASSWORD")
 myDomain = os.getenv("DOMAIN")
 myPort = os.getenv("PORT")
 myPath = os.getenv("MYPATH")
 
+# read the config.ini file to get the ID for the given device
 config = configparser.ConfigParser()
 config.read(f"{myPath}/config.ini")
 string = config['smart'][str(sys.argv[1])]
+
+#first get auth token
 headers = {
     'accept': '*/*',
     'Content-Type': 'application/json',
@@ -28,6 +31,7 @@ stop = 329
 slice_object = slice(start,stop)
 result = str(response.text)[slice_object]
 
+#Create auth header
 Bear = 'Bearer '
 Auth = ''.join([Bear, result])
 Headers = {
@@ -38,6 +42,7 @@ Headers = {
 url = f"https://{myDomain}:{myPort}/api/accessories/"
 device = str(string)
 Url = ''.join([url, device])
+#Make request and then find the section needed to read the devices state
 json_data = requests.get(Url, headers=Headers).json()
 dataList = json_data.values()
 newList = list(dataList)[8]
